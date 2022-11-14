cac82c8a056b3ea28cbce45ff57aabbb1f8a5369
Tristan Camejo <contact@tristancamejo.com>
2022-11-14 14:23:12 +1100

    fix: match history

--- src/lib/shapers/ShapedMatchHistory.ts
+++ src/lib/shapers/ShapedMatchHistory.ts
@@ -2,24 +2,42 @@ import { MatchHistoryDataV3, Team } from '../types/types';
 import type { AgentUsed } from './types';
 
 export default class ShapedMatchHistory {
-	public constructor(private readonly raw: MatchHistoryDataV3[], private readonly puuid: string) {}
-	public round = (num: number): number => Math.round((num + Number.EPSILON) * 100) / 100;
+	public round: (num: number) => number;
+	public trackedGames: number;
 
-	public get trackedGames(): number {
-		return this.raw.length;
-	}
-
-	public overview = {
-		kills: this.kills,
-		deaths: this.deaths,
-		kdr: this.round(this.killDeathRatio),
-		wins: this.gamesWon,
-		losses: this.gamesLost,
-		winRatio: this.round(this.winLossRatio),
-		agentsUsed: this.agentsUsed.sort((a, b) => b.timesUsed - a.timesUsed),
+	public overview!: {
+		kills: number;
+		deaths: number;
+		kdr: number;
+		wins: number;
+		losses: number;
+		winRatio: number;
+		agentsUsed: AgentUsed[];
 	};
 
-	private get agentsUsed(): AgentUsed[] {
+	private agentsUsed: AgentUsed[];
+	private killDeathRatio: number;
+	private winLossRatio: number;
+	private gamesWon: number;
+	private gamesLost: number;
+	public deaths: number;
+
+	private kills: number;
+
+	public constructor(private readonly raw: MatchHistoryDataV3[], private readonly puuid: string) {
+		this.deaths = this.raw
+			.map((match) => match.players.all_players)
+			.reduce((acc, curr) => {
+				const player = curr.find((p) => p.puuid === this.puuid);
+				if (player) {
+					return acc + player.stats.deaths;
+				}
+				return acc;
+			}, 0);
+
+		this.round = (num: number): number => Math.round((num + Number.EPSILON) * 100) / 100;
+		this.trackedGames = this.raw.length;
+
 		const agents: AgentUsed[] = [];
 		this.raw.map((match) => {
 			const player = match.players.all_players.find((player) => player.puuid === this.puuid);
@@ -34,30 +52,7 @@ export default class ShapedMatchHistory {
 				});
 			}
 		});
-		return agents;
-	}
 
-	private get killDeathRatio(): number {
-		if (this.kills === 0) {
-			return 0;
-		}
-		if (this.deaths === 0) {
-			return 1;
-		}
-		return this.kills / this.deaths;
-	}
-
-	private get winLossRatio(): number {
-		if (this.gamesWon === 0) {
-			return 0;
-		}
-		if (this.gamesLost === 0) {
-			return 1;
-		}
-		return this.gamesWon / this.gamesLost;
-	}
-
-	private get gamesWon(): number {
 		let wins = 0;
 
 		this.raw.map((match) => {
@@ -81,10 +76,9 @@ export default class ShapedMatchHistory {
 				}
 			}
 		});
-		return wins;
-	}
 
-	private get gamesLost(): number {
+		this.gamesWon = wins;
+
 		let losses = 0;
 
 		this.raw.map((match) => {
@@ -108,11 +102,12 @@ export default class ShapedMatchHistory {
 				}
 			}
 		});
-		return losses;
-	}
 
-	private get kills(): number {
-		return this.raw
+		this.gamesLost = losses;
+
+		this.agentsUsed = agents;
+
+		this.kills = this.raw
 			.map((match) => match.players.all_players)
 			.reduce((acc, curr) => {
 				const player = curr.find((p) => p.puuid === this.puuid);
@@ -121,17 +116,32 @@ export default class ShapedMatchHistory {
 				}
 				return acc;
 			}, 0);
-	}
 
-	public get deaths(): number {
-		return this.raw
-			.map((match) => match.players.all_players)
-			.reduce((acc, curr) => {
-				const player = curr.find((p) => p.puuid === this.puuid);
-				if (player) {
-					return acc + player.stats.deaths;
-				}
-				return acc;
-			}, 0);
+		if (this.kills === 0) {
+			this.killDeathRatio = 0;
+		} else if (this.deaths === 0) {
+			this.killDeathRatio = 1;
+		} else {
+			this.killDeathRatio = this.kills / this.deaths;
+		}
+
+		if (this.gamesWon === 0) {
+			this.winLossRatio = 0;
+		}
+		if (this.gamesLost === 0) {
+			this.winLossRatio = 1;
+		}
+
+		this.winLossRatio = this.gamesWon / this.gamesLost;
+
+		this.overview = {
+			kills: this.kills,
+			deaths: this.deaths,
+			kdr: this.round(this.killDeathRatio),
+			wins: this.gamesWon,
+			losses: this.gamesLost,
+			winRatio: this.round(this.winLossRatio),
+			agentsUsed: this.agentsUsed.sort((a, b) => b.timesUsed - a.timesUsed),
+		};
 	}
 }
